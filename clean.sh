87a11742ea13c9351eace49f290a6a0dbf550d3f
Madhukar Arora <madhukar.arora@colorado.edu>
2020-04-03 21:42:05 -0600

    Setting up Buildroot for BBB

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#Author : Madhukar Arora
+#Description : runs make distclean from the buildroot directory
+#Course : Advanced Embedded Software Development Spring 2020
+
+cd buildroot
+make distclean
+
