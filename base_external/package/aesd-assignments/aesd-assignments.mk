899174c105c193b3987680bfd5f88861e5b826b9
Akshita Bhasin <akshita.bhasin@colorado.edu>
2020-04-07 00:21:39 -0600

    added hello world file

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,8 +6,8 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = f018febea456356387cbfea9992bab85eb66fc83
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/assignment-3-manual-kernel-and-root-filesystem-build-madhukararora.git
+AESD_ASSIGNMENTS_VERSION = 290efad6d8d15e026df7555958a8d5d5edea40a4
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-akshita-bhasin.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
 
@@ -17,9 +17,10 @@ endef
 
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/tester.sh $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/bin
+	# $(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
+	# $(INSTALL) -m 0755 $(@D)/tester.sh $(TARGET_DIR)/bin
+	# $(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/hello_world $(TARGET_DIR)/bin
 endef
 
 
