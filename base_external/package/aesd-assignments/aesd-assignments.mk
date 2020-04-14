2f43aa7b90d7fc41e52b4fb626383962a7a51f84
Akshita Bhasin <akshita.bhasin@colorado.edu>
2020-04-14 12:34:25 -0600

    TMP102 working and tested on Linux-i2c

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,8 +5,8 @@
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = ac229eee63889455ff8d9423ffde32817173a588
+#TODO: Fill up the contents below in order to reference aesd project details
+AESD_ASSIGNMENTS_VERSION = 3b45c874fcaf044f21027eb9e04729821c496b11
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-akshita-bhasin.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -17,12 +17,9 @@ endef
 
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	# $(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/bin
-	# $(INSTALL) -m 0755 $(@D)/tester.sh $(TARGET_DIR)/bin
-	# $(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/hello_world $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/uart $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/tmp102 $(TARGET_DIR)/bin
 endef
-
 
 $(eval $(generic-package))
