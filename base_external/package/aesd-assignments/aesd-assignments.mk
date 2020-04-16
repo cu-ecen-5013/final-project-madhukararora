9af309887b60674b943660d7df003dd8753d7b7e
Akshita Bhasin <akshita.bhasin@colorado.edu>
2020-04-16 17:25:59 -0600

    UART between Tiva and BBB up and running

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference aesd project details
-AESD_ASSIGNMENTS_VERSION = 3b45c874fcaf044f21027eb9e04729821c496b11
+AESD_ASSIGNMENTS_VERSION = ae5791ddb0ae0f4c08a444d85a15215f9b663300
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-akshita-bhasin.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -17,9 +17,8 @@ endef
 
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/hello_world $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/environmental_monitoring $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/uart $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/tmp102 $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
