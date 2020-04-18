e75a24d8ba735a56b86870f1efc22eb7299ed81b
Akshita Bhasin <akshita.bhasin@colorado.edu>
2020-04-18 05:41:31 -0600

    LED interfaced

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference aesd project details
-AESD_ASSIGNMENTS_VERSION = 30b2dd72d74e3eb2ac5f71d93df06600d9274300
+AESD_ASSIGNMENTS_VERSION = 4a0590ba3ef7fa5e93231cd2ff373f0e10ce1b7c
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-akshita-bhasin.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -20,6 +20,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/environmental_monitoring $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/shtc3 $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/tmp102 $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/led_test $(TARGET_DIR)/bin
+
 endef
 
 $(eval $(generic-package))
