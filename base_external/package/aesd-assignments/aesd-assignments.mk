a0de98cd73a4617287fb5fdc1d3e066fdfd16c0e
Akshita Bhasin <akshita.bhasin@colorado.edu>
2020-04-18 23:15:15 -0600

    Actuators integrated in application

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference aesd project details
-AESD_ASSIGNMENTS_VERSION = e6897fc5597665379df2b66be1ceb4cf69ecd24d
+AESD_ASSIGNMENTS_VERSION = 02487b3cb5c9fed1f44f38e6460986d277beea7d
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-akshita-bhasin.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -19,8 +19,6 @@ endef
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/environmental_monitoring $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/shtc3 $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/tmp102 $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/led_test $(TARGET_DIR)/bin
 
 endef
 
