182fac1106dab61db076636865a3f9417e13fda7
Madhukar Arora <madhukar.arora@colorado.edu>
2020-04-19 01:31:09 -0600

    Added server to buildroot, verified on sockettest

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference aesd project details
-AESD_ASSIGNMENTS_VERSION = 02487b3cb5c9fed1f44f38e6460986d277beea7d
+AESD_ASSIGNMENTS_VERSION = 1d2a185da61dc3647f085c66f1a496593b629a59
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-akshita-bhasin.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -19,6 +19,7 @@ endef
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/environmental_monitoring $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/shtc3 $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/server $(TARGET_DIR)/bin
 
 endef
 
