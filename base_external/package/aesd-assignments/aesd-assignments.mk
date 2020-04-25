6e4077922003495e0f218b9072e513ed6f69988c
Akshita Bhasin <akshita.bhasin@colorado.edu>
2020-04-25 16:02:41 -0600

    Working shared memory implementation

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,10 +6,9 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference aesd project details
-AESD_ASSIGNMENTS_VERSION = de3b362774e36a7cb625a7a89970622c79e5fd5d
+AESD_ASSIGNMENTS_VERSION = 5e8c6143a986e7ae9e37eb9c8e6c6d9db72b6922
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-akshita-bhasin.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
-
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
@@ -20,6 +19,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/environmental_monitoring $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/shtc3 $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/client $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/ambient $(TARGET_DIR)/bin
 
 endef
 
