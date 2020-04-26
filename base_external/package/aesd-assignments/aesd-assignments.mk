c13642cbca2dbe10c7d7aa616ca34cd9a0a1d10d
Akshita Bhasin <akshita.bhasin@colorado.edu>
2020-04-26 08:15:16 -0600

    Integration without socket complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference aesd project details
-AESD_ASSIGNMENTS_VERSION = 5e8c6143a986e7ae9e37eb9c8e6c6d9db72b6922
+AESD_ASSIGNMENTS_VERSION = cdf14a5bea10f1bc6ba458185d3b8e6a577b0996
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-5013/final-project-akshita-bhasin.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
@@ -17,7 +17,7 @@ endef
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/environmental_monitoring $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/shtc3 $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/server $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/client $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/ambient $(TARGET_DIR)/bin
 
